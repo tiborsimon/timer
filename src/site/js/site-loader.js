af9c672ef2daefc03daed96de6fdde9cdd4b81de
Tibor Simon <tibor@tiborsimon.io>
2016-10-10 22:09:52 +0200

    Site frame and loading refactored.

--- src/site/js/site-loader.js
+++ src/site/js/site-loader.js
@@ -1,18 +1,20 @@
 'use strict';
-function loaderDone(event) {
-  var loader = document.getElementById("loader");
-  loader.style.opacity = 0;
-  setTimeout(function(){
-    var wrapper = document.getElementById("wrapper");
-    wrapper.style.marginTop = 0;
-    wrapper.style.opacity = 1;
+(function(){
+  function loaderDone(event) {
+    var loader = document.getElementById("loader");
+    loader.style.opacity = 0;
     setTimeout(function(){
-      loader.style.display = "none";
-    }, 800);
-  }, 300);
-}
-setTimeout(function() {
-  if(document.readyState === 'complete') {
-    loaderDone();
+      var wrapper = document.getElementById("wrapper");
+      wrapper.style.marginTop = 0;
+      wrapper.style.opacity = 1;
+      setTimeout(function(){
+        loader.style.display = "none";
+      }, 800);
+    }, 300);
   }
-}, 100);
+  document.onreadystatechange = function () {
+    if(document.readyState === 'complete') {
+      loaderDone();
+    }
+  };
+})();
